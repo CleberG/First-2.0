2586661ab88eb2dc554d6bfae5f136d0ca2fd1c8
vitor paulo <virtorpaulo333019@gmail.com>
2020-10-27 23:10:58 -0300

    add Model Base

--- First2.0.Application/Models/UsuarioModel/UsuarioResponseModel.cs
+++ First2.0.Application/Models/UsuarioModel/UsuarioResponseModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace First2._0.Application.Models.UsuarioModel
+﻿namespace First2._0.Application.Models.UsuarioModel
 {
     public class UsuarioResponseModel
         : UsuarioModelBase
     {
-        public Guid Id { get; set; }
     }
 }
