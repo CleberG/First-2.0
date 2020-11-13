86e05e0c8a251ca55a214532f223885ccb4043a2
joaoPSCampos <joao.campos@hbsis.com.br>
2020-11-13 19:28:52 -0300

    Add-Migration

--- First2.0.Infra/Migrations/20201113222708_Historico.Designer.cs
+++ First2.0.Infra/Migrations/20201113222708_Historico.Designer.cs
@@ -4,14 +4,16 @@ using First2._0.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace First2._0.Infra.Migrations
 {
     [DbContext(typeof(MainContext))]
-    partial class MainContextModelSnapshot : ModelSnapshot
+    [Migration("20201113222708_Historico")]
+    partial class Historico
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
